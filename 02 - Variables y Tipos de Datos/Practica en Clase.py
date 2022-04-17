e62896f63c210f6592249872914f53519db11519
Rodrigo_LaptopI3_600U <rodrigogutierrez273@gmail.com>
2022-04-17 03:40:21 -0300

    realizando el repo

--- 02 - Variables y Tipos de Datos/Practica en Clase.py
+++ 02 - Variables y Tipos de Datos/Practica en Clase.py
@@ -1,3 +1,96 @@
-mi_variable = 12
+from queue import PriorityQueue
+
+
+mi_variable = "HOLA "
 print(mi_variable)
+
+#2 
+print(type(8.5))
+
+#3
+print(type(mi_variable))
+
+#4
+miname = "rodrigo"
+
+#5
+complejo = 5j
+
+#6
+print(type(complejo))
+
+#7
+PI= 3.1415
+
+#8
+var_whit_str = "True"
+var_whit_value = True
+
+#9
+print(type(var_whit_str))
+print(type(var_whit_value))
+
+#10
+suma_entero_with_decimal = 5 + 5.4
+print(suma_entero_with_decimal)
+
+#11
+print(5j + 10j)
+
+#12
+print(5 + 10j)
+
+#13
+print(3*2)
+
+#14
+print(2**8)
+
+#15
+cociente = 27/4
+print(cociente)
+
+#16
+cociente = int(cociente)
+print(cociente)
+
+#17
+only_resto = 27%4
+print(only_resto)
+
+#18
+recuperando = 4*6 + 3
+print(recuperando)
+
+#19
+print(mi_variable + miname)
+
+#20
+if ("2" == 2):
+    print(True)
+
+#21
+if (int("2") == 2):
+    print("funciona ")
+
+#22
+print(float(3.8))
+print("arroja error xq el 3,8 estaba escrito con una coma y no con un punto")
+
+#23
+var_number3 = 3
+var_number3 -=2
+print(var_number3)
+
+#24
+print(1 << 3)
+
+#25
+print(2+ 2)
+
+#26
+var1 = "camaleon "
+var2 = 3
+
+print(var1 * 3 + str(var2))
 
